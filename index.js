9d262f085d787ef8003e86c1ad5760f194f23b7e
visheshguria <vishesh.guriaa94@gmail.com>
2022-09-11 19:08:16 +0530

    changes done

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.getElementsByClassName("button-resume").addEventListener("click", function() {
+document.querySelector(".button-resume").addEventListener("click", function() {
         window.location.href = "https://drive.google.com/file/d/1sZqTWfFo2LjEf4PRE58ivxCE-zCi2yvw/view?usp=sharing"
 })
