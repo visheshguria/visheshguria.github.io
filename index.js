ce9f2259eec4aca011e4ac2d222eb82318c1d43e
visheshguria <vishesh.guriaa94@gmail.com>
2022-09-15 21:07:01 +0530

    Completed Portfolio

--- index.js
+++
@@ -1,3 +0,0 @@
-document.querySelector(".button-resume").addEventListener("click", function() {
-        window.location.href = "https://drive.google.com/file/d/1IBOZo_moVDM34Y0lPvQbFKhLlZIURzaV/view?usp=sharing"
-})
